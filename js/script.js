368ac627a5606f0b7674ef54b415c950a52d1cab
Chris-karanja <chriswainaina0@gmail.com>
2018-05-07 11:48:05 +0300

    realised i never added the right path for my js query

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
+var userNumbers = [];
+
 var printNumbers = function(userNumber){
   for(var x = 1; x <= userNumber; x++){
     if(x % 3 === 0 && x % 5 !== 0 && x % 15 !== 0){
-        userNumbers.push("ping");
-      }else if (x % 5 === 0 && x % 3 !== 0 && x % 15 !== 0) {
-        userNumbers.push("pong");
-      }else if (x % 3 === 0 && x % 5 === 0 && x % 15 === 0) {
-          userNumbers.push("ping-pong");
-        }else {
-          userNumbers.push(x);
-        }
-      }
-    };
-    // user interface logic
-     $(document).ready(function() {
-     $("form#user-number").submit(function(event) {
-         event.preventDefault();
-         var userNumber = parseInt($("input#number").val());
-         var result = printNumbers(userNumber);
+      userNumbers.push("ping");
+    }else if (x % 5 === 0 && x % 3 !== 0 && x % 15 !== 0) {
+      userNumbers.push("pong");
+    }else if (x % 3 === 0 && x % 5 === 0 && x % 15 === 0) {
+      userNumbers.push("ping-pong");
+    }else {
+      userNumbers.push(x);
+    }
+  }
+};
+// user interface logic
+ $(document).ready(function() {
+   $("form#user-number").submit(function(event) {
+     event.preventDefault();
+     var userNumber = parseInt($("input#number").val());
+     var result = printNumbers(userNumber);
 
-         $.each(userNumbers, function(index, value){
-         $("#output").append((index + 1) + ": " + value + '<br>');
-     });
+     $.each(userNumbers, function(index, value){
+            $("#output").append((index + 1) + ": " + value + '<br>');
+        });
 
-  $("#output").show();
-});
-});
+     $("#output").show();
+   });
+ });
