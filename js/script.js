a15ffa6627830d3b9e222453733cbd9baf080eeb
Chris-karanja <chriswainaina0@gmail.com>
2018-05-07 11:10:43 +0300

    run the code on the console and theres no outcome so i went over thecodes and edited the missing equal sign

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var printNumbers = function(userNumber){
   for(var x = 1; x <= userNumber; x++){
-    if(x % 3 === 0 && x % 5 != 0 && x % 15 !== 0){
+    if(x % 3 === 0 && x % 5 !== 0 && x % 15 !== 0){
         userNumbers.push("ping");
       }else if (x % 5 === 0 && x % 3 !== 0 && x % 15 !== 0) {
         userNumbers.push("pong");
